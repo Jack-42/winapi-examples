a19bcc41293ec05953d12011c807cdab822da454
Jack-42 <jakob-robert@hotmail.de>
2020-10-18 15:47:47 +0200

    Refactor hello-winapi
    Extract functions etc.

--- hello-winapi/src/main.c
+++ hello-winapi/src/main.c
@@ -4,59 +4,79 @@
 
 #include <windows.h>
 
-#define CLASS_NAME TEXT("main")
 #define APP_NAME TEXT("HelloWinAPI")
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+BOOL createWindowClass(WNDCLASS* wndClass, HINSTANCE hInstance);
+HWND createWindow(WNDCLASS* wndClass, int width, int height, HINSTANCE hInstance);
+int runMessageLoop();
+
+LRESULT CALLBACK wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+void drawText(HWND hWnd, LPCWSTR text);
 
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine, _In_ int nCmdShow
 )
 {
-	// create window class
-	WNDCLASS wndClass = {0};
-	wndClass.style = CS_HREDRAW | CS_VREDRAW; // sends WM_PAINT message for each window resize or movement
-	wndClass.lpfnWndProc = WndProc;
-	wndClass.cbClsExtra = 0;
-	wndClass.cbWndExtra = 0;
-	wndClass.hInstance = hInstance;
-	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndClass.lpszMenuName = NULL;
-	wndClass.lpszClassName = CLASS_NAME;
-
-	if (!RegisterClass(&wndClass)) {
-		MessageBox(NULL, TEXT("Failed to register window class!"), APP_NAME, MB_ICONERROR);
+	WNDCLASS wndClass;
+	if (!createWindowClass(&wndClass, hInstance)) {
+		MessageBox(NULL, TEXT("Failed to create window class!"), APP_NAME, MB_ICONERROR);
 		return 1;
 	}
 
-	// create window
-	HWND hWnd = CreateWindow(
-		CLASS_NAME, APP_NAME, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, // default position
-		WINDOW_WIDTH, WINDOW_HEIGHT,
-		NULL, NULL, hInstance, NULL
-	);
-
+	HWND hWnd = createWindow(&wndClass, 800, 600, hInstance);
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd); // sends a WM_PAINT message
 
-	// message loop
+	return runMessageLoop();
+}
+
+BOOL createWindowClass(WNDCLASS* wndClass, HINSTANCE hInstance)
+{
+	ZeroMemory(wndClass, sizeof(WNDCLASS));
+
+	wndClass->style = CS_HREDRAW | CS_VREDRAW; // sends WM_PAINT message for each window resize or movement
+	wndClass->lpfnWndProc = wndProc;
+	wndClass->cbClsExtra = 0;
+	wndClass->cbWndExtra = 0;
+	wndClass->hInstance = hInstance;
+	wndClass->hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass->hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass->hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndClass->lpszMenuName = NULL;
+	wndClass->lpszClassName = TEXT("main");
+
+	if (!RegisterClass(wndClass)) {
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+HWND createWindow(WNDCLASS* wndClass, int width, int height, HINSTANCE hInstance)
+{
+	return CreateWindow(
+		wndClass->lpszClassName, APP_NAME,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, // default position
+		width, height,
+		NULL, NULL, hInstance, NULL
+	);
+}
+
+int runMessageLoop()
+{
 	MSG msg;
-	// loop continues until WM_QUIT message is received, then the return code is 0
+	// loop continues until WM_QUIT message is received
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-	// msg.wParam contains the exit code
+	// for WM_QUIT message, wParam contains the exit code
 	return (int)msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg) {
 		case WM_CREATE: {
@@ -69,22 +89,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			return 0;
 		}
 		case WM_PAINT: {
-			PAINTSTRUCT paint;
-			RECT rect;
-
-			// draw text in center of window
-			GetClientRect(hWnd, &rect);
-			HDC hdc = BeginPaint(hWnd, &paint);
-			DrawText(
-				hdc,
-				TEXT("Hello, WinAPI!"), -1, // -1 for length, i.e. that the string is null-terminated
-				&rect,
-				DT_SINGLELINE | DT_CENTER | DT_VCENTER
-			);
-			EndPaint(hWnd, &paint);
-
+			drawText(hWnd, TEXT("Hello, WinAPI!"));
 			return 0;
 		}
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+void drawText(HWND hWnd, LPCWSTR text)
+{
+	PAINTSTRUCT paint;
+	RECT rect;
+
+	// draw text in center of window
+	GetClientRect(hWnd, &rect);
+	HDC hdc = BeginPaint(hWnd, &paint);
+	DrawText(
+		hdc,
+		text, -1, // -1 for length because string is null-terminated
+		&rect,
+		DT_SINGLELINE | DT_CENTER | DT_VCENTER
+	);
+	EndPaint(hWnd, &paint);
 }
